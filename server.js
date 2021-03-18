fe43565f6e5a9b5554e06002e5b2fca8b182505c
chris <chrischang1204@gmail.com>
2021-03-17 22:48:58 -0700

    upodate

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./movie-music-app'));
+app.use(express.static('./dist/movie-music-app'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'movie-music-app>/'}
+    res.sendFile('index.html', {root: 'dist/movie-music-app\src'}
   );
   });
 
