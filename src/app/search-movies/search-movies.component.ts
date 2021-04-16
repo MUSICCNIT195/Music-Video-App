2c24cdd7d15f15c284155933f4d9ba60d3e44588
sean <sappelba@mail.ccsf.edu>
2021-04-15 22:06:46 -0700

    fixed app logic, added createUrlMovie/URI encoder

--- src/app/search-movies/search-movies.component.ts
+++ src/app/search-movies/search-movies.component.ts
@@ -15,16 +15,17 @@ export class SearchMoviesComponent implements OnInit {
   ok = false;
   name: any;
 
-  baseUrl: string = 'https://api.themoviedb.org/3/search/movie?';
+  baseUrl= 'https://api.themoviedb.org/3/search/movie?';
 
-  key: string = '0a4252617bfe9d39fa9d115728b16c43'; //api key will be fixed for this assignment
-  searchFor: string;
-  fullUrl: string = '';
+  apikey2 = '0a4252617bfe9d39fa9d115728b16c43'; //api key will be fixed for this assignment
+   searchfor ='';
+  fullUrl= '';
   constructor(private http: HttpClient, private myformBuilder: FormBuilder) {
     this.emailForm = this.myformBuilder.group({
       name: ['', Validators.required],
-      searchFor: ['', Validators.required],
+      //searchFor: ['', Validators.required],
     });
+   
   }
 
   //Final url: https://api.themoviedb.org/3/search/movie?query=James%20bond&api_key=0a4252617bfe9d39fa9d115728b16c43
@@ -33,24 +34,13 @@ export class SearchMoviesComponent implements OnInit {
    * name
    */
 
-  ngOnInit(): void {}
-  doGET() {
-    console.log('GET');
-    this.searchFor=this.emailForm.get('name').value;
-    console.log("search term: " + this.searchFor);
-    let search = new URLSearchParams();
-    search.set('title', this.searchFor);
+  ngOnInit(): void {
     
-      const asString =encodeURIComponent(this.searchFor).toString();;
-      //this.searchFor=asString;
-  
-      console.log("encoded searh term: " +asString);
-      this.fullUrl =
-      this.baseUrl + 'query=' + asString + '&api_key=' + this.key;
-
-      console.log('Full url: ' + this.fullUrl);
+  }
+  doGET(value) {// pass url here 
+    
     console.log(
-      this.http.get(this.fullUrl ).subscribe((res) => console.log(res))
+     this.http.get(value ).subscribe((res) => console.log(res))
     );
     
   }
@@ -63,6 +53,18 @@ export class SearchMoviesComponent implements OnInit {
       return; /* no code will be executed after this point */
     }
     this.ok = true;
-    this.searchFor = this.emailForm.get('name').value();
+    this.searchfor = this.emailForm.get('name').value;
+    console.log("On submit Method");
+    console.log(this.createUrlMovie(this.searchfor));
+    this.doGET(this.createUrlMovie(this.searchfor));
+  }
+  //after they submit the word search
+  createUrlMovie(ss){
+    const asString =encodeURIComponent(ss).toString();
+    //console.log("search term: " + this.searchFor);
+    let search = new URLSearchParams();
+    search.set('title', this.searchfor);
+    //console.log("URL" + this.baseUrl + 'query=' + asString + '&api_key=' + this.apikey2);
+     return this.baseUrl + 'query=' + asString + '&api_key=' + this.apikey2;
   }
 }
